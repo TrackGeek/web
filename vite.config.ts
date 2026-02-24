3afab66e50261cb1e151b209725225c76da6aae9
izakdvlpr <izakdvlpr@gmail.com>
2026-02-24 14:49:28 -0300

    refactor(vite): add tanstack devtools and vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,18 @@
-import path from "node:path";
 import { viteImage } from "@son426/vite-image/plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import { devtools } from '@tanstack/devtools-vite'
 import { defineConfig } from "vite";
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
+    devtools(),
+    tanstackRouter({ target: "react", autoCodeSplitting: true, }),
+    tsconfigPaths({ projects: ['./tsconfig.json'] }),
     react(),
     tailwindcss(),
     viteImage(),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });

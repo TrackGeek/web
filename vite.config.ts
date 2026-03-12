0562a939cca209772caecd3aac70e83c77fffc28
Bruno Ramos <37979213+Kuriel23@users.noreply.github.com>
2026-03-12 21:44:54 +0000

    feat: reduce bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,15 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
+  build: {
+    rolldownOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        },
+      },
+    },
+  },
 });

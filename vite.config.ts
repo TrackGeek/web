b3870f3a65252df3417a33ad69b95a5f629deeec
Bruno Ramos <37979213+Kuriel23@users.noreply.github.com>
2026-03-12 19:50:48 +0000

    feat: use vite 8 and config new tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { viteImage } from "@son426/vite-image/plugin";
 import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import { devtools } from "@tanstack/devtools-vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     devtools(),
     tanstackRouter({ target: "react", autoCodeSplitting: true }),
-    tsconfigPaths({ projects: ["./tsconfig.json"] }),
     react(),
     tailwindcss(),
     viteImage(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
